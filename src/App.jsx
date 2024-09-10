706dfe2cf91a50bef8c7fe69d49e50924c732584
tanbiralam <itzmetanbir@gmail.com>
2024-09-10 15:15:09 +0530

    features section completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 import Model from "./components/Model";
+import Features from "./components/Features";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <Highlights />
       <Model />
+      <Features />
     </main>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,20 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-import * as Sentry from "@sentry/react";
-
-Sentry.init({
-  dsn: "https://2ac69e774881509dd2d8e35983b9050d@o4507913127067648.ingest.de.sentry.io/4507913130147920",
-  integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration(),
-  ],
-  tracesSampleRate: 1.0,
-  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
-});
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
